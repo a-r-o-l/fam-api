77effde9257a4cdde44f03814f7b1842e88e6559
arol <arol@blueverse.club>
2023-12-24 00:45:43 -0300

    ps added

--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
 import express,{Express, Response, Request} from 'express';
+import pg from "pg"
+import {config  } from "dotenv";
+
+config()
 
 const app: Express = express();
 
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl:true
+})
+
 app.get('/', (req:Request, res: Response) => {
     res.send('Hello World');
+});
+
+app.get('/ping', async(req:Request, res: Response) => {
+    const result = await pool.query("SELECT NOW()")
+    return res.json(result.rows[0])
 });
  
 app.listen(3000, () => {
