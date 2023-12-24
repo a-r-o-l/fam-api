aef7863fb13d3923aa3e890eae645d3a59949dce
arol <arol@blueverse.club>
2023-12-24 16:10:09 -0300

    fix hostname

--- src/database/database.ts
+++ src/database/database.ts
@@ -9,7 +9,7 @@ export const sequelize = new Sequelize(
     process.env.DATABASE_USER || "postgres",
     process.env.DATABASE_PASSWORD || "postgres",
     {
-        host: process.env.DATABASE_URL || "localhost",
+        host: process.env.DATABASE_HOST || "localhost",
         port: parseInt(process.env.DATABASE_PORT || "5432"),
         dialect: "postgres",
         logging: false,
