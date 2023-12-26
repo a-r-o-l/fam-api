e1d1e62b21ce66923f905c065a4262b9969881b8
arol <arol@blueverse.club>
2023-12-26 01:06:03 -0300

    fix as 1

--- src/models/Renter.ts
+++ src/models/Renter.ts
@@ -32,5 +32,5 @@ export const Renter = sequelize.define("Renter", {
     updatedAt: true
 })
 
-Renter.hasMany(Contract, {foreignKey: "renterId",as:'renter', sourceKey: "id"})
+Renter.hasMany(Contract, {foreignKey: "renterId", sourceKey: "id"})
 Contract.belongsTo(Renter, {foreignKey: "renterId",as:'renter', targetKey: "id"})
