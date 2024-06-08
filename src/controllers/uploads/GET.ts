b8667d2b66f65f1690d0aff6616d2b302f0c833d
arol <arol@blueverse.club>
2024-06-08 03:28:53 -0300

    lc8

--- src/controllers/uploads/GET.ts
+++ src/controllers/uploads/GET.ts
@@ -3,15 +3,24 @@ import path from "path";
 import { Request, Response } from "express";
 
 export const getImages = async (req: Request, res: Response) => {
-  const directoryPath = process.env.RAILWAY_MOUNT_PATH || "./app/images";
+  if (!process.env.RAILWAY_VOLUME_MOUNT_PATH) {
+    return res.status(500).json({ message: "No existe variable de entorno" });
+  }
+
+  const dirPath = process.env.RAILWAY_VOLUME_MOUNT_PATH;
+
   try {
-    const files = await fs.promises.readdir(directoryPath);
-    const images = files.map(
+    const files = await fs.promises.readdir(dirPath);
+    const imageFiles = files.filter(
+      (file) => path.extname(file).toLowerCase() === ".jpg"
+    );
+    const imageUrls = imageFiles.map(
       (file) => `https://fam-api-production.up.railway.app/app/images/${file}`
     );
-    res.json(images);
+    console.log(imageUrls);
+    res.status(200).json({ images: imageUrls });
   } catch (err) {
     console.log(err);
-    res.status(500).send("Error al leer el directorio de imágenes");
+    res.status(500).send("Error al obtener las imágenes");
   }
 };
