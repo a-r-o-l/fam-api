41c6c41a6463ffb740d0d0aa8f06ed7a67d0626a
arol <arol@blueverse.club>
2024-08-09 19:05:39 -0300

    fix start script command

--- config/config.js
+++ config/config.js
@@ -2,11 +2,24 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
+    username: process.env.DB_USERNAME || "your_username",
+    password: process.env.DB_PASSWORD || "your_password",
+    database: process.env.DB_NAME || "your_database",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "postgres",
+  },
+  test: {
+    username: process.env.DB_USERNAME || "your_username",
+    password: process.env.DB_PASSWORD || "your_password",
+    database: process.env.DB_NAME || "your_database",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.DB_USERNAME || "your_username",
+    password: process.env.DB_PASSWORD || "your_password",
+    database: process.env.DB_NAME || "your_database",
+    host: process.env.DB_HOST || "127.0.0.1",
     dialect: "postgres",
   },
 };
