15225737a00ecfeeefb2919bd8409af62c34ec2b
arol <arol@blueverse.club>
2024-08-21 12:40:41 -0300

    several changes 1

---
+++ migrations/20240815000023-add-booking-field-in-reservation-model.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn("Reservation", "booking", {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      defaultValue: 0,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn("Reservation", "booking");
+  },
+};
